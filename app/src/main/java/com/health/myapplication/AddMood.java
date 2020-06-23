0263641cb0eebb80d6232ba42085c7821c624118
s.esmaili <s.esmaili30@gmail.com>
2020-06-23 10:31:56 +0430

    Zahra

---
+++ app/src/main/java/com/health/myapplication/AddMood.java
@@ -0,0 +1,25 @@
+package com.health.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.icu.util.UniversalTimeScale;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+
+public class AddMood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_mood);
+
+
+    }
+
+}
