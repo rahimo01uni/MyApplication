eec4c6803a3e7be1792a4a50071046440734b9b5
s.esmaili <s.esmaili30@gmail.com>
2020-06-24 15:44:22 +0430

    Zahra

---
+++ app/src/main/java/com/health/myapplication/GroupMember.java
@@ -0,0 +1,28 @@
+package com.health.myapplication;
+
+
+public class GroupMember {
+    private String Title;
+
+    public GroupMember(){
+    }
+
+
+    public GroupMember(String title){
+        Title = title;
+
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+
+}
+
---
+++ app/src/main/java/com/health/myapplication/MemberRecycleviewAdapter.java
@@ -0,0 +1,71 @@
+package com.health.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MemberRecycleviewAdapter  extends RecyclerView.Adapter<MemberRecycleviewAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<GroupMember> mData;
+    private OnEditLisener mOnEditListener;
+    public MemberRecycleviewAdapter(Context mContext, List<GroupMember> mData, OnEditLisener onEditLisener) {
+        this.mContext = mContext;
+        this.mData = mData;
+        this.mOnEditListener = onEditLisener;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        LayoutInflater mInflater = LayoutInflater.from(mContext);
+        view = mInflater.inflate(R.layout.team_customview, parent, false);
+
+        return new MyViewHolder(view, mOnEditListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.group_title.setText(mData.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView group_title;
+        OnEditLisener onEditLisener;
+
+        public MyViewHolder(View itemView, OnEditLisener onEditLisener) {
+            super(itemView);
+            group_title = (TextView) itemView.findViewById(R.id.txt_mem);
+
+            this.onEditLisener = onEditLisener;
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            onEditLisener.onMemberClick(getAdapterPosition());
+
+        }
+    }
+
+    public interface OnEditLisener{
+        void onMemberClick(int position);
+    }
+}
+
+
